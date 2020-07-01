b866fab0fbeb8792b2894f6f782c933584d642d5
Seamus Lee <seamus.lee@jmaconsulting.biz>
2020-07-01 07:40:14 +0000

    Update to production custom field ids

--- volunteerreference.constant.php
+++ volunteerreference.constant.php
@@ -4,6 +4,6 @@ define('REF_EMAIL1', 'custom_53');
 define('REF_NAME1', 'custom_52');
 define('REF_EMAIL2', 'custom_56');
 define('REF_NAME2', 'custom_55');
-define('REF_EMAIL3', 'custom_109');
-define('REF_NAME3', 'custom_107');
+define('REF_EMAIL3', 'custom_86');
+define('REF_NAME3', 'custom_85');
 define('REFERENCE_PROFILE', 27);
